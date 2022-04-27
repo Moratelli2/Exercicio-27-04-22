cee24b55650deb0dc9d41bb96db6e90b12ab5904
Moratelli2 <80839440+Moratelli2@users.noreply.github.com>
2022-04-27 20:53:21 -0300

    Add files via upload

---
+++ EstacionamentoCarros/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from EstacionamentoCarros.models import *
+
+# Register your models here.
+admin.site.register(Carro)
+admin.site.register(Cliente)
+admin.site.register(Vendendor)
+admin.site.register(Fornecedor)
+admin.site.register(Seguranca)
+admin.site.register(Vaga)
